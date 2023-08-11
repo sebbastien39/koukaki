76220bdce1b37765089c7c69ca6ff6120cbcb43c
sebbastien39 <sebbastien39@gmail.com>
2023-08-11 16:31:59 +0200

    get_stylesheet enfant

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -2,6 +2,8 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    // Chargement feuille de style du thème enfant "foce-child"
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
 }
 
 // Get customizer options form parent theme
