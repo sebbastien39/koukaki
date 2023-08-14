76267f8a428155c580c85f202819be5d9882f163
sebbastien39 <sebbastien39@gmail.com>
2023-08-14 21:35:08 +0200

    Rotation 2 premières fleurs

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -3,7 +3,8 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     // Chargement feuille de style du thème enfant "foce-child"
-    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.scss', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.scss'));
+    wp_enqueue_script('theme-js', get_stylesheet_directory_uri() . '/assets/js/script.js', array(),time(), true);
 }
 
 // Get customizer options form parent theme
---
+++ wp-content/themes/foce-child/templates/nomination-festival.php
@@ -0,0 +1,6 @@
+<section id="studio">
+            <div>
+                <p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
+                <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
+            </div>
+        </section>
